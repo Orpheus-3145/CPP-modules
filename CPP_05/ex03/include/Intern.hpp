62e002aa1d04bf64f0080c34e8b6f614f3939368
fra <francesco.aru25@gmail.com>
2023-10-14 02:13:33 +0200

    module finished

--- CPP_05/ex03/include/Intern.hpp
+++ CPP_05/ex03/include/Intern.hpp
@@ -6,7 +6,7 @@
 /*   By: fra <fra@student.codam.nl>                   +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2023/10/13 19:22:46 by fra           #+#    #+#                 */
-/*   Updated: 2023/10/13 19:50:23 by fra           ########   odam.nl         */
+/*   Updated: 2023/10/14 01:57:47 by fra           ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,7 +27,10 @@ namespace brt
 class brt::Intern
 {
 	public:
-		virtual ~Intern( void ) throw() {};
+		Intern ( void ) throw() {}
+		Intern ( brt::Intern const& ) throw() {}
+		virtual ~Intern( void ) throw() {}
+		Intern& operator=( Intern const& ) throw() {return (*this);}
 		brt::AForm*	makeForm( std::string const&, std::string const& ) const 
 			throw();
 };
